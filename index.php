ab82343890bd89ddd864954909eb53a18720d2ef
Marios Tsekitsidis <tsekitsi@grinnell.edu>
2017-02-05 12:18:57 +0200

    Έγραψα τ' όνομά μου, yay!

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo "hello Vaggelis";
+            echo "hello this is Marios";
             
         ?>
     </body>
