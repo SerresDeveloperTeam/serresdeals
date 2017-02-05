6a845a7fce3e55d84ba87acf32047bcd6c3607e3
Marios Tsekitsidis <tsekitsi@grinnell.edu>
2017-02-05 12:43:14 +0200

    Replace o with !

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo "hello this is Marios";
+            echo "hello this is Mario!!!!";
             
         ?>
     </body>
