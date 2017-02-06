eba19fc68f2d3dd5b882d33b33b33b17bea57112
Marios Tsekitsidis <tsekitsi@grinnell.edu>
2017-02-06 15:33:13 +0200

    Άχριστο php και στο signup page.

--- signup.php
+++ signup.php
@@ -1,19 +1,10 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
         <title>KopseKati - Εγγραφή</title>
     </head>
     <body>
-        <?php
-        echo"<html>
-            <a href='index.php'>Αρχική</a>
-        ";
-        ?>
+        <a href='index.php'>Αρχική</a>
     </body>
 </html>
