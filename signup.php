ac03fc6bc6f640d053aa9b125074403a51e63cc0
Marios Tsekitsidis <tsekitsi@grinnell.edu>
2017-02-06 23:49:23 +0200

    Logo!

--- signup.php
+++ signup.php
@@ -3,8 +3,14 @@
     <head>
         <meta charset="UTF-8">
         <title>KopseKati - Εγγραφή</title>
+        <link rel='stylesheet' type='text/css' href='stylesheets/index.css' />
     </head>
     <body>
-        <a href='index.php'>Αρχική</a>
+        <div class='intro'>
+            <a href='signup.php'><input class='connection' type='button' value='Εγγραφή/Σύνδεση'></a>
+        </div><br/>
+        <div class='intro'>
+            <div style='margin-left:20%'><a href='index.php'><img src='images/kopse10_min.png'></img></a></div>
+        </div>
     </body>
 </html>
