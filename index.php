a8228fdd4a451a3a6a8e456a67274844f47b35dc
Lia <Lia@Lia-PC.zte.com.cn>
2017-02-08 14:14:29 +0200

    Εβαλα για το καθε προιον κατι απλο border etc

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
-<!DOCTYPE html>
-<html>
+<?php
+$host="localhost";
+$user="root";
+$pass="";
+$database="KopseKati";
+mysql_connect($host,$user,$pass);
+mysql_select_db($database) or die("Unable to select database");
+mysql_query("SET NAMES 'utf8'"); 
+$c=0;
+$query="select * from prosfores";
+echo"<html>
     <head>
-        <meta charset="UTF-8">
+        <meta charset='UTF-8'>
         <title>KopseKati - Αρχική</title>
         <link rel='stylesheet' type='text/css' href='stylesheets/index.css' />
     </head>
@@ -29,6 +38,31 @@
                 currentSlide = (currentSlide+1)%slides.length;
                 slides[currentSlide].className = 'slide showing';
             }
-        </script>
-    </body>
-</html>
+        </script><br/><br/><br/>";
+		
+        $users_all=mysql_query($query);
+		
+	while($users_row=mysql_fetch_array($users_all)) {
+            if($c == 3){
+             echo "<tr>";
+               $c=0;
+            }
+        echo"
+         <div class='intro'>
+         <table>
+         <tr>
+         <th><img class='eikona' src=".$users_row["eikona"]."></th>
+         <th><h3 class='onoma'>".$users_row["onoma"]."</h3>
+         <i><p class='plhr'>".$users_row["plhrofories"]."</p></i>
+         <del class='firstprice'>Από ".$users_row["timh"]."€</del>
+         <strong class='finalprice'>Τελική τιμή ".$users_row["telikhtimh"]."€</strong><br/></th></tr></table></div>";
+          $c++;
+        }
+         
+            
+            
+
+  echo"
+</body>
+</html>";
+?>
